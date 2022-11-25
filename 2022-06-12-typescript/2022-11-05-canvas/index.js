932fa3a04fb834c8e3676b386a864eb536fd3a1e
lldite <lldite@gmail.com>
2022-11-25 14:57:04 +0200

    first canvas code

--- 2022-06-12-typescript/2022-11-05-canvas/index.js
+++ 2022-06-12-typescript/2022-11-05-canvas/index.js
@@ -1,15 +1,28 @@
+// compile me with `npx tsc -w index.ts` ("-w(atches)" / reacts to changes in this file)
 var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = "rgb(200, 0, 0)";
-ctx.fillRect(10, 10, 500, 50); // x, y, width, height
-ctx.fillStyle = "rgb(200, 60, 0)";
-ctx.fillRect(20, 20, 500, 50);
-ctx.fillStyle = "rgb(200, 110, 0)";
-ctx.fillRect(30, 30, 500, 50);
-ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-// Array.from(Array(10).keys()).forEach(n => {
-//     const m = n * 20
-//     ctx.fillRect(m + 30, m + 30, 50, 50)
-// })
+// alpha values from 0 (transparent to 1 opaque
+// ctx.fillStyle = "rgba(0, 0, 0, 0.1)" // red, green, blue, alpha (transparency)
+// ctx.fillRect(10, 10, 500, 50) // x of left side of rect, y of left side of rect, width, height
+// ============================
+function first() {
+    ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
+    ctx.fillRect(10, 10, 500, 50); // x, y, width, height
+}
+function klucitis(centrs, n) {
+    ctx.fillRect(centrs[0] - n, centrs[1] - n, n * 2, n * 2);
+}
+function piramida(centrs, platums, solis) {
+    for (var i = platums; i > 0; i = i - solis) {
+        klucitis(centrs, i);
+    }
+}
+function piramidas() {
+    ctx.fillStyle = "rgba(0, 0, 0, 0.03)";
+    piramida([200, 200], 100, 1);
+    piramida([400, 200], 70, 20);
+    piramida([600, 200], 100, 10);
+}
+piramidas();
